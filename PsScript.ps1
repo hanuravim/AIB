059df10485a15f66467ad20a5731c2a33d052ef6
ravi mishra <42548254+hanuravim@users.noreply.github.com>
2019-05-23 14:06:15 -0500

    Update PsScript.ps1

--- PsScript.ps1
+++ PsScript.ps1
@@ -1,2 +1,1 @@
 New-Item –Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows\Installer"
-New-ItemProperty "HKCU:\SOFTWARE\Policies\Microsoft\Windows\Installer" -Name "AlwaysInstallElevated" -Value 0 -PropertyType "DWord"
